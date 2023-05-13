9fe4d03dae301d7c24450c4def0d61b92c28b318
root <root@jonathan-pc.localdomain>
2023-05-14 02:48:17 +0300

    added all classes functionality to the console

--- console.py
+++ console.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models import storage
+import json
 
 """This is the class for the interactive console where the user enters commands"""
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    valid_classes = ["BaseModel" , "User"]
+    valid_classes = ["BaseModel" , "User", "State", "City", "Amenity", "Place", "Review"]
     def do_quit(self, line):
         """When the quit command is pressed the terminal is closed and the program is terminated"""
         return True
@@ -30,7 +37,30 @@ class HBNBCommand(cmd.Cmd):
                 new_instance.save()
                 print (new_instance.id)
             elif args[0] == "User":
-                pass
+                new_user = User()
+                new_user.save()
+                print (new_user.id)
+            elif args[0] == "State":
+                new_state = State()
+                new_state.save()
+                print (new_state.id)
+            elif args[0] == "City":
+                new_city = City()
+                new_city.save()
+                print (new_city.id)
+            elif args[0] == "Amenity":
+                new_amenity = Amenity()
+                new_amenity.save()
+                print (new_amenity.id)
+            elif args[0] == "Place":
+                new_place = Place()
+                new_place.save()
+                print (new_place.save())
+            elif args[0] == "Review":
+                new_review = Review()
+                new_review.save()
+                print (new_review.id)
+                
 
     def do_show(self, line):
         """Prints the string representation of an instance based on the class name and id. Ex: $ show BaseModel 1234-1234-1234."""
@@ -59,12 +89,12 @@ class HBNBCommand(cmd.Cmd):
         if len(args) == 0:
             print ("** class name missing **")
             return False
-        if args[0] == "BaseModel":
+        if args[0] in __class__.valid_classes:
             if len(args) > 1:
                 key = args[0] + "." + args[1]
                 if key in storage.all():
                     storage.all().pop(key)
-                    storage.all.save()
+                    storage.save()
                 else:
                     print("** no instance found **")
             else:
@@ -78,13 +108,16 @@ class HBNBCommand(cmd.Cmd):
         """Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all"""
         args = line.split()
         if len(args) > 0:
-            if args[0] != "BaseModel":
+            if args[0] not in __class__.valid_classes:
                 print ("** class doesn't exist **")
                 return False
             else:
-                obj_dict = json.dumps(storage.all())
-        print (obj_dict)
-
+                str_list = [str(obj) for key, obj in storage.all().items()if type(obj).__name__ == args[0]]
+                print (str_list)
+        else:
+            new_list = [str(obj) for key, obj in storage.all().items()]
+            print(new_list)
+            
     def do_update(self, line):
         """ Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file). Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com"."""
         args = line.split()
@@ -105,13 +138,6 @@ class HBNBCommand(cmd.Cmd):
             print ("** attribute name missing **")
         elif len(args) == 3:
             print ("** value missing **")
-
-
-
-
-
-
-
 
 """Code should not be executed when imported."""    
 if __name__ == '__main__':
